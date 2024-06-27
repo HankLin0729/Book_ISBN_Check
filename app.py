669cc5d37800088bca69757c6c8c65f4b9d1c216
HankLin0729 <77linhank@gmail.com>
2024-06-28 00:12:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,21 @@ app = Flask(__name__)
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# 如果上傳文件夾不存在，則創建它
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+# 顯示上傳表單
 @app.route('/')
 def upload_form():
     return render_template('upload.html')
 
+# 處理文件上傳
 @app.route('/', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return jsonify({'isbns': [], 'debug_infos': ['No file part in the request'], 'image_paths': []})
+    
     files = request.files.getlist('file')
     file_paths = []
     for file in files:
@@ -27,13 +31,17 @@ def upload_file():
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(file_path)
             file_paths.append(file_path)
+    
+    # 使用 ZXing 解碼條形碼
     isbns, debug_infos = decode_barcodes_with_zxing(file_paths)
     return jsonify({'isbns': isbns, 'debug_infos': debug_infos, 'image_paths': [url_for('uploaded_file', filename=os.path.basename(path)) for path in file_paths]})
 
+# 顯示上傳的文件
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+# 使用 ZXing 解碼條形碼的函數
 def decode_barcodes_with_zxing(image_paths):
     zxing_core_jar = os.path.join('libs', 'core-3.4.0.jar')
     zxing_javase_jar = os.path.join('libs', 'javase-3.4.0.jar')
@@ -49,6 +57,7 @@ def decode_barcodes_with_zxing(image_paths):
         debug_info = f'Running command: {command}<br>'
         
         try:
+            # 執行命令行指令
             process = subprocess.run(command, capture_output=True, text=True, shell=True)
             output = process.stdout
             debug_info += 'Command output:<br>' + output.replace('\n', '<br>')
@@ -70,10 +79,12 @@ def decode_barcodes_with_zxing(image_paths):
             debug_infos.append(debug_info)
             isbns.append("Error occurred")
 
+    # 將 ISBN 添加到 Excel 文件中
     add_isbns_to_excel(isbns, debug_infos)
     
     return isbns, debug_infos
 
+# 將 ISBN 添加到 Excel 文件的函數
 def add_isbns_to_excel(isbns, debug_infos):
     file_name = 'data.xlsx'
     if not os.path.exists(file_name):
